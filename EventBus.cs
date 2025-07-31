6b817dc8cad0f8e8190e635d4317ddbc9524a763
LiLGarbanso <121341859+LiLGarbanso@users.noreply.github.com>
2025-07-31 10:14:37 +0200

    Update EventBus.cs

--- EventBus.cs
+++ EventBus.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 * Sistema para crear e invocar eventos. Hay que definir el evento y sus parámetros
 * y crear una función para invocarlo. Los objetos que se quieran suscribir a este
 * evento, simplemente añadirán la FuncionCallback que quierán que responda al evento
-* en el OnEnable() y desuscribirla en el OnDisable(). Para invocar el evnto, se debe
+* en el OnEnable() y desuscribirla en el OnDisable(). Para invocar el evento, se debe
 * llamar a EventBus.LanzarEventoX(parametros) para invocarlo.
 */
 public static class EventBus 
@@ -30,7 +30,7 @@ public static class EventBus
 	//--------Declaración de eventos--------//
 	
 	//Con parámetros
-    public static event Action<GameObject> OnEvent1, OnEvent2, OnEvent3;
+    public static event Action<Object> OnEvent1, OnEvent2, OnEvent3;
 	
 	//Sin parámetros
     public static event Action OnEvent4, OnEvent5;
