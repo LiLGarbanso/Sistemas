f9615e03a7098aa0ecb9843131d352930e7bdf88
LiLGarbanso <121341859+LiLGarbanso@users.noreply.github.com>
2025-08-16 23:56:29 +0200

    biblioteca

--- Sistema Interactuables/IInteractable.cs
+++ Sistema Interactuables/IInteractable.cs
@@ -1,6 +1,11 @@
+using UnityEngine;
+/*
+ * Interfaz que define un elemento con el que se puede interactuar
+ */
 public interface IInteractable
 {
     void Interact(GameObject interactor);	//Reacción del objeto al interactuar
     string GetPrompt();	//Por si queremos mostrar algún tipo de mensaje antes de interactuar
     int Priority { get; } // Cuanto mayor, más importante
+
 }
